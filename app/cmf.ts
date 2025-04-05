c48150f94aae87d2bf4030303f21c4b4172b3d3c
Masaki Hara <ackie.h.gmai@gmail.com>
2025-04-05 16:10:28 +0900

    Impl cmf

--- app/cmf.ts
+++ app/cmf.ts
@@ -1,3 +1,25 @@
+export type Observer =
+  | "DEG2"
+  | "DEG10";
+
+export function cmf(observer: Observer, lam: number): [number, number, number] {
+  const a = observer === "DEG10" ? CMF_RAW_DEG10 : CMF_RAW_DEG2;
+  const lamRelative = lam - a[0][0];
+  const idx = Math.floor(lamRelative);
+  if (idx === lamRelative) {
+    const [, xb, yb, zb] = idx >= 0 && idx < a.length ? a[idx] : [0, 0, 0, 0];
+    return [xb, yb, zb];
+  } else {
+    const frac = lamRelative - idx;
+    const [, xb0, yb0, zb0] = idx >= 0 && idx < a.length ? a[idx] : [0, 0, 0, 0];
+    const [, xb1, yb1, zb1] = idx + 1 >= 0 && idx + 1 < a.length ? a[idx + 1] : [0, 0, 0, 0];
+    const xb = xb0 + (xb1 - xb0) * frac;
+    const yb = yb0 + (yb1 - yb0) * frac;
+    const zb = zb0 + (zb1 - zb0) * frac;
+    return [xb, yb, zb];
+  }
+}
+
 // Colour-matching functions and chromaticity coordinates of CIE 1931 standard colorimetric observer
 // (from ISO/CIE 11664-1:2019, Table A.1)
 // prettier-ignore
@@ -954,3 +976,14 @@ const CMF_RAW_DEG10: [number, number, number, number, number, number, number][] 
   [829, 0.000_001_647_10 , 0.000_000_667_48 , 0                , 0.711_62, 0.288_38, 0       ],
   [830, 0.000_001_553_14 , 0.000_000_629_70 , 0                , 0.711_52, 0.288_48, 0       ],
 ];
+
+function assertConsecutive(a: number[]) {
+  const s = a[0];
+  for (let i = 1; i < a.length; i++) {
+    if (a[i] !== s + i) {
+      throw new Error(`Non-consecutive index at ${i} (value: ${a[i]})`);
+    }
+  }
+}
+assertConsecutive(CMF_RAW_DEG2.map(([lam]) => lam));
+assertConsecutive(CMF_RAW_DEG10.map(([lam]) => lam));
